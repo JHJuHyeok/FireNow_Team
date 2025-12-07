d23e4b23083d1f8f6b05e755285725a79ba9f99f
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2025-12-08 03:18:08 +0900

    feat : 장비 SO 데이터 스크립트 작성

---
+++ Assets/Scripts/Ju/SO/EquipData.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+[System.Serializable]
+public class EquipData
+{
+    public string id;                       // 장비 ID
+    public string name;                     // 장비 명칭
+    public string descript;                 // 장비 설명
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public EquipPart part;                  // 장비의 부위
+
+    public List<EquipGrade> equipGrades;    // 각 등급의 데이터
+}
+[System.Serializable]
+public class EquipGrade
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public Grade grade;             // 장비 등급
+    public int startValue;          // 레벨업 안 한 능력치
+    public string descript;         // 등급별 설명
+}
+
+public enum Grade
+{
+    normal,
+    rare,
+    elite,
+    epic,
+    legend
+}
+public enum EquipPart
+{
+    weapon,
+    necklace,
+    glove,
+    armor,
+    belt,
+    shoes
+}
