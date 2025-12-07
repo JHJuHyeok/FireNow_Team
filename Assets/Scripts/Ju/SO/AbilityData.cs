77f77e86ea98da6c864ef92fde83648975cbad91
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2025-12-07 21:14:59 +0900

    feat : 무기, 패시브 통합 SO 데이터 작성

---
+++ Assets/Scripts/Ju/SO/AbilityData.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AbilityData
+{
+    public string id;           // 아이템 식별 ID
+    public AbilityType type;    // 아이템의 타입
+    public string name;         // UI에 표시될 이름
+    public string icon;         // UI에 표시될 아이콘
+    public int maxLevel;        // 최대 레벨
+
+    // 각 레벨의 데이터
+    public List<AbilityLevelData> levels;
+    // 진화 관련 데이터
+    public WeaponEvolution evolution;
+}
+
+[System.Serializable]
+public class AbilityLevelData       // 레벨 데이터
+{
+    // 타입이 무기일 때
+    public float damage;            // 데미지
+    public float cooldown;          // 쿨다운
+    public int projectileCount;     // 투사체 개수
+    public float range;             // 범위
+    public float speed;             // 속도
+    public float duration;          // 공격 간격
+
+    // 타입이 패시브일 때
+    public float rangeIncrease;     // 범위 상승
+    public float damageIncrease;    // 공격력 상승
+    public float maxHPIncrease;     // 최대 체력 상승
+    public float defenceDecrease;   // 받는 피해량 감소
+    public float durationDecrease;  // 공격 간격 감소
+    public float getEXPIncrease;    // 경험치 획득량 상승
+    public float getGoldIncrease;   // 골드 획득량 상승
+
+    // 업그레이드 시 표시될 설명
+    public string description;
+}
+
+[System.Serializable]
+public class WeaponEvolution        // 진화 데이터
+{
+    public string requireItem;      // 진화 시 필요한 패시브 아이템
+    public string result;           // 진화 후 무기
+}
+
+public enum AbilityType             // 능력 타입
+{
+    weapon,
+    passive,
+    evolution
+}
