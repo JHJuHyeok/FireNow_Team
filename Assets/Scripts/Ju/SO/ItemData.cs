20809f7343e3be608a6f6a11d2d540cd344b171b
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2025-12-07 22:18:52 +0900

    feat : 아이템 SO 데이터 작성

---
+++ Assets/Scripts/Ju/SO/ItemData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ItemData
+{
+    public string id;           // 아이템 ID
+    public string name;         // 아이템 이름
+    public string itemSprite;   // 아이템 이미지 경로
+    public bool magnet;         // 자석 효과 적용 여부
+    public int expValue;        // 획득하는 경험치
+}
