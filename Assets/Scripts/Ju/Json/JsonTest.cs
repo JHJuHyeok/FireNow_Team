a4fc3824b5a83fbf3921bebb137c079643bb56f7
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2025-12-12 15:15:33 +0900

    feat : Json 파일 호출, 로그 메시지 출력하는 테스트용 스크립트

---
+++ Assets/Scripts/Ju/Json/JsonTest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonTest : MonoBehaviour
+{
+    private EquipData equip;
+
+    private void Start()
+    {
+        equip = EquipLoader.Load();
+
+        Debug.Log($"장비 이름 : {equip.equipName}");
+        Debug.Log($"장비 설명 : {equip.descript}");
+        Debug.Log($"아이콘 경로 : {equip.iconPath}");
+        Debug.Log($"장착 부위 : {equip.part}");
+
+        Debug.Log($"노말 등급의 최대 레벨 : {equip.equipGrades[0].maxLevel}");
+    }
+}
