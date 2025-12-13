cdf58a32ffbc751d8174c40388463ffdeca29481
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2025-12-13 17:16:52 +0900

    feat : 소지품 직렬화 스크립트 작성

---
+++ Assets/Scripts/Ju/Serializable/StuffData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class StuffData
+{
+    public string id;           // 소지품 ID
+    public string stuffName;    // 소지품 이름
+    public string iconPath;     // 아이콘 경로
+    public string descript;     // 소지품 설명
+
+    public Grade grade;         // 소지품 등급
+}
