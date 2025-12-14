cf10d4c800dc6aff0580b39c14aa1db377bf9c8e
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2025-12-14 19:41:23 +0900

    feat : 런타임 중 상태를 저장하기 위한 SO 스크립트 생성

---
+++ Assets/Scripts/Ju/PlayerInfo/PlayerInfoSO.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Player/PlayerInfo")]
+public class PlayerInfoSO : ScriptableObject
+{
+    public int gold;                // 소지 골드
+    public int gem;                 // 소지 보석
+    public int stamina;             // 현재 스태미나
+    public int maxStamina;          // 최대 스태미나
+
+    public List<EquipData> equips = new List<EquipData>();      // 장비 목록
+    public List<StuffStack> items = new List<StuffStack>();     // 소지품 목록
+    //public List<EvolveData> evolves = new List<EvolveData>(); // 해금된 진화 목록
+}
+
+[System.Serializable]
+public class StuffStack
+{
+    //public StuffData stuff;
+    public int amount;
+}
