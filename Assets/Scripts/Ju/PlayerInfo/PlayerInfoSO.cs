678b305f7386cf38513615c2a55a05ec66ca869a
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2025-12-14 21:42:46 +0900

    fix : PlayerInfoSO 스크립트 변수명, 주석 처리 일부 수정

--- Assets/Scripts/Ju/PlayerInfo/PlayerInfoSO.cs
+++ Assets/Scripts/Ju/PlayerInfo/PlayerInfoSO.cs
@@ -11,13 +11,13 @@ public class PlayerInfoSO : ScriptableObject
     public int maxStamina;          // 최대 스태미나
 
     public List<EquipData> equips = new List<EquipData>();      // 장비 목록
-    public List<StuffStack> items = new List<StuffStack>();     // 소지품 목록
+    public List<StuffStack> stuffs = new List<StuffStack>();     // 소지품 목록
     //public List<EvolveData> evolves = new List<EvolveData>(); // 해금된 진화 목록
 }
 
 [System.Serializable]
 public class StuffStack
 {
-    //public StuffData stuff;
+    public StuffData stuff;
     public int amount;
 }
