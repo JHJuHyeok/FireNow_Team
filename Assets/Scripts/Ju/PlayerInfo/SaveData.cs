e3a56a7e2986352d30c147b319b21ccf9117ee59
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2025-12-15 01:25:35 +0900

    feat : 저장내용 명시용 직렬화 스크립트 생성

--- Assets/Scripts/Ju/PlayerInfo/SaveData.cs
+++ Assets/Scripts/Ju/PlayerInfo/SaveData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 재화 및 보유 품목 저장 데이터
 [System.Serializable]
 public class SaveData
 {
@@ -13,21 +14,24 @@ public class SaveData
     public long lastStaminaTime;
 
     //public List<EquipSaveData> equips = new List<EquipSaveData>();
-    //public List<StuffSaveData> stuffs = new List<StuffSaveData>();
+    public List<StuffSaveData> stuffs = new List<StuffSaveData>();
     //public List<EvolveSaveData> evolves = new List<EvolveSaveData>();
 }
 
+// 보유 소지품 저장 데이터
 [System.Serializable]
 public class StuffSaveData
 {
     public string stuffID;
     public int amount;
 }
+// 보유 장비 저장 데이터
 [System.Serializable]
 public class EquipSaveData
 {
     public string equipID;
 }
+// 해금 진화 저장 데이터
 [System.Serializable]
 public class EvolveSaveData
 {
