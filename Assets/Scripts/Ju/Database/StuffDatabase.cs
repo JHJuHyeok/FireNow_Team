5a8417fd00067162a80a7d7e541a94df93ce3dda
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2025-12-15 16:41:42 +0900

    fix : 소지품 데이터베이스를 보유 목록이 아닌 전체로 확장

--- Assets/Scripts/Ju/Database/StuffDatabase.cs
+++ Assets/Scripts/Ju/Database/StuffDatabase.cs
@@ -8,38 +8,34 @@ using UnityEngine;
 public class StuffDatabase : MonoBehaviour
 {
     // 소지품 목록 저장용 딕셔너리
-    private Dictionary<string, StuffDataRuntime> stuffDict = new();
+    private static Dictionary<string, StuffData> stuffDict;
 
     /// <summary>
-    /// JSON 데이터에서 소지품 데이터베이스 불러오기
+    /// 게임 시작 시 소지품 데이터베이스 불러오기
     /// </summary>
-    /// <param name="json"> 불러올 소지품 데이터베이스 Json 데이터</param>
-    public void LoadFromJson(string json)
+    public static void Initialize()
     {
-        StuffDatabaseDTO db = JsonUtility.FromJson<StuffDatabaseDTO>(json);
+        stuffDict = new Dictionary<string, StuffData>();
 
-        // 불러오기 전 소지품 정리
-        stuffDict.Clear();
+        TextAsset[] jsonFiles = Resources.LoadAll<TextAsset>("Json/StuffData");
 
-        // [ID, 소지품 데이터] 형태로 저장된 목록 불러오기
-        foreach(var data in db.stuffs)
+        foreach (var file in jsonFiles)
         {
-            StuffDataRuntime stuff = new StuffDataRuntime(data);
-            stuffDict[stuff.id] = stuff;
+            StuffData data = JsonUtility.FromJson<StuffData>(file.text);
+            stuffDict[data.id] = data;
         }
-
-        // 총 몇 개 불러왔는지 확인용 로그
-        Debug.Log($"StuffDatabase Loaded: {stuffDict.Count} stuff");
     }
 
     /// <summary>
-    /// SaveManager에서 소지품 불러오기 함수
+    /// SaveManager에서 보유 소지품 불러오기
     /// </summary>
-    /// <paramname="id"> 불러올 소지품 ID </param>
-    /// <returns> ID 값에 대응하는 StuffDataRuntime 데이터 </returns>
-    public StuffDataRuntime GetStuff(string id)
+    /// <paramname="id"> 세이브 데이터로 불러올 소지품 ID </param>
+    /// <returns> ID 값에 대응하는 StuffData 데이터 </returns>
+    public static StuffData GetStuff(string id)
     {
-        stuffDict.TryGetValue(id, out var stuff);
-        return stuff;
+        if(stuffDict.TryGetValue(id, out var stuff))
+            return stuff;
+
+        return null;
     }
 }
