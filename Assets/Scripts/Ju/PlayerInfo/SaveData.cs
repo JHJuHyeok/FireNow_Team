54eea4db96aaff16d8f92a908ba35a2384b876a1
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2025-12-15 17:11:13 +0900

    fix : 세이브데이터 내 무기 세이브데이터 추가

--- Assets/Scripts/Ju/PlayerInfo/SaveData.cs
+++ Assets/Scripts/Ju/PlayerInfo/SaveData.cs
@@ -13,7 +13,7 @@ public class SaveData
 
     public long lastStaminaTime;
 
-    //public List<EquipSaveData> equips = new List<EquipSaveData>();
+    public List<EquipSaveData> equips = new List<EquipSaveData>();
     public List<StuffSaveData> stuffs = new List<StuffSaveData>();
     //public List<EvolveSaveData> evolves = new List<EvolveSaveData>();
 }
@@ -30,6 +30,8 @@ public class StuffSaveData
 public class EquipSaveData
 {
     public string equipID;
+    public Grade grade;
+    public int level;
 }
 // 해금 진화 저장 데이터
 [System.Serializable]
