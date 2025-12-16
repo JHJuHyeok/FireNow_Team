b04af260cbc9a271f11fb2485c1100efebaa7bf3
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2025-12-16 15:01:06 +0900

    fix : playerInfoSO 스크립트 merge confliction 해결

--- Assets/Scripts/Ju/PlayerInfo/PlayerInfoSO.cs
+++ Assets/Scripts/Ju/PlayerInfo/PlayerInfoSO.cs
@@ -12,14 +12,10 @@ public class PlayerInfoSO : ScriptableObject
     public int maxStamina;          // 최대 스태미나
 
     public List<EquipData> equips = new List<EquipData>();      // 장비 목록
-<<<<<<< HEAD
     public List<StuffStack> stuffs = new List<StuffStack>();     // 소지품 목록
     //public List<EvolveActive> evolves = new List<EvolveData>(); // 해금된 진화 목록
-=======
-    public List<StuffStack> stuffs = new List<StuffStack>();    // 소지품 목록
     public List<StageClear> stages = new List<StageClear>();    // 클리어한 스테이지 목록
     //public List<EvolveData> evolves = new List<EvolveData>(); // 해금된 진화 목록
->>>>>>> feat/stageJson
 }
 
 /// <summary>
@@ -31,14 +27,13 @@ public class StuffStack
     public StuffDataRuntime stuff;
     public int amount;
 }
-<<<<<<< HEAD
 
 [System.Serializable]
 public class EvolveActive
 {
 
     public bool isActive;
-=======
+}
 /// <summary>
 /// 스테이지 클리어 여부 확인
 /// </summary>
@@ -47,5 +42,4 @@ public class StageClear
 {
     public StageDataRuntime stage;
     public bool isClear;
->>>>>>> feat/stageJson
 }
