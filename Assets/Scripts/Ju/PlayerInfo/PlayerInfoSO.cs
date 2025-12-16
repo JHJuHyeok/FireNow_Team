5c63c3f5873027bb3d2ed4cc67f6b7c6760017dc
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2025-12-16 13:21:47 +0900

    feat : 런타임 중 활용할 스테이지 런타임 데이터 작성

--- Assets/Scripts/Ju/PlayerInfo/PlayerInfoSO.cs
+++ Assets/Scripts/Ju/PlayerInfo/PlayerInfoSO.cs
@@ -12,7 +12,8 @@ public class PlayerInfoSO : ScriptableObject
     public int maxStamina;          // 최대 스태미나
 
     public List<EquipData> equips = new List<EquipData>();      // 장비 목록
-    public List<StuffStack> stuffs = new List<StuffStack>();     // 소지품 목록
+    public List<StuffStack> stuffs = new List<StuffStack>();    // 소지품 목록
+    public List<StageClear> stages = new List<StageClear>();    // 클리어한 스테이지 목록
     //public List<EvolveData> evolves = new List<EvolveData>(); // 해금된 진화 목록
 }
 
@@ -24,4 +25,13 @@ public class StuffStack
 {
     public StuffDataRuntime stuff;
     public int amount;
+}
+/// <summary>
+/// 스테이지 클리어 여부 확인
+/// </summary>
+[System.Serializable]
+public class StageClear
+{
+    public StageData stage;
+    public bool isClear;
 }
