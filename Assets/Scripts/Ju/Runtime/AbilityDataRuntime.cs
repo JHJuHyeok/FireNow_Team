d1c12608e7b408be0968153d8d4b8bb2bff084f2
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2025-12-21 21:26:34 +0900

    feat : 액티브/패시브 런타임 데이터 생성

---
+++ Assets/Scripts/Ju/Runtime/AbilityDataRuntime.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityDataRuntime
+{
+    public string id;                           // 아이템 식별 ID
+    public string name;                         // UI에 표시될 이름
+    public Sprite sprite;                       // 아틀라스에서 불러올 스프라이트 이름
+    public int maxLevel;                        // 최대 레벨
+
+    public AbilityType type;                    // 아이템의 타입
+
+    public List<AbilityLevelData> levels;       // 각 레벨의 데이터
+
+    public WeaponEvolution evolution;           // 진화 관련 데이터
+
+    public AbilityDataRuntime(AbilityData data)
+    {
+        id = data.id;
+        name = data.name;
+        sprite = AtlasManager.GetSprite("", data.spriteName);
+        maxLevel = data.maxLevel;
+
+        type = data.type;
+        levels = data.levels;
+        evolution = data.evolution;
+    }
+}
