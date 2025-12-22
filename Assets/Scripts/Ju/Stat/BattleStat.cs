31e9089bf86dae8991411cf9276d8599f4e37970
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2025-12-22 15:58:19 +0900

    feat : 전투 시 사용될 최종 스탯 스크립트 작성

---
+++ Assets/Scripts/Ju/Stat/BattleStat.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleStat
+{
+    public StatValue maxHP = new();             // 최대 체력
+    public StatValue attack = new();            // 공격력
+    public StatValue defence = new();           // 방어력
+    public StatValue cooldown = new();          // 무기 쿨다운
+    public StatValue moveSpeed = new();         // 움직이는 속도
+    public StatValue duration = new();          // 효과 지속시간
+    public StatValue getExp = new();            // EXP 획득량
+    public StatValue projectileSpeed = new();   // 투사체 속도
+    public StatValue range = new();             // 공격 범위
+    public StatValue healHP = new();
+
+    /// <summary>
+    /// 게임 시작 전 각 스탯의 상승치 초기화
+    /// </summary>
+    public void ClearRuntimeStats()
+    {
+        maxHP.ClearStat();
+        attack.ClearStat();
+        cooldown.ClearStat();
+        moveSpeed.ClearStat();
+        duration.ClearStat();
+        getExp.ClearStat();
+        projectileSpeed.ClearStat();
+        range.ClearStat();
+        healHP.ClearStat();
+    }
+}
