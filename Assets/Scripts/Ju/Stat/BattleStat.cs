6b6cbc46b1629ae4352d54da5c9f75c4d5d8813e
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2025-12-23 11:46:32 +0900

    comment : 코드 가시성을 위해 변수 정렬, 설명용 주석 일부 추가

--- Assets/Scripts/Ju/Stat/BattleStat.cs
+++ Assets/Scripts/Ju/Stat/BattleStat.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class BattleStat
 {
+    // 기본 스탯
     public StatValue maxHP = new();             // 최대 체력
     public StatValue attack = new();            // 공격력
     public StatValue defence = new();           // 방어력
-    public StatValue cooldown = new();          // 무기 쿨다운
     public StatValue moveSpeed = new();         // 움직이는 속도
+    public StatValue getHPWithMeat = new();     // 고기 섭취시 회복량
+
+    // 패시브 스탯(기본값은 1)
+    public StatValue cooldown = new();          // 무기 쿨다운
     public StatValue duration = new();          // 효과 지속시간
     public StatValue getExp = new();            // EXP 획득량
-    public StatValue projectileSpeed = new();   // 투사체 속도
     public StatValue range = new();             // 공격 범위
+    public StatValue projectileSpeed = new();   // 투사체 속도
     public StatValue healHPOnSecond = new();    // 초당 hp 회복량
-    public StatValue getHPWithMeat = new();     // 고기 섭취시 회복량
 
     /// <summary>
     /// 게임 시작 전 각 스탯의 상승치 초기화
