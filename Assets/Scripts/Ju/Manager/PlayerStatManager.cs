8946157a5ba04c1b9762f1b36bf05d0598d68774
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2025-12-24 06:07:20 +0900

    feat : 무기 스탯, 패시브 스탯, 전투 스탯을 연결하는 매니저

---
+++ Assets/Scripts/Ju/Manager/PlayerStatManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatManager
+{
+    public BattleStat battleStat { get; private set; }
+
+    /// <summary>
+    /// 플레이어 스탯 매니저 생성자
+    /// </summary>
+    public PlayerStatManager()
+    {
+        battleStat = new BattleStat();
+    }
+
+    /// <summary>
+    /// 모든 무기 스탯 재계산
+    /// </summary>
+    /// <param name="weapons"></param>
+    public void RecalculateAllStats(List<Weapon> weapons, PassiveManager passiveManager)
+    {
+        // 플레이어 스탯에 패시브 적용
+        passiveManager.ApplyAll(battleStat);
+
+        // 무기에 플레이어 스탯 적용
+        foreach(var weapon in weapons)
+        {
+            weapon.RecalculateStat(battleStat);
+        }
+    }
+}
