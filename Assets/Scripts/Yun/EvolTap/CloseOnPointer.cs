730303e4544d134af981177036bad99947cee77a
ghost-backseat-driver <ysl2323@naver.com>
2025-12-28 16:52:10 +0900

    feat : 재석님 QA 수정사항 반영 - 상점탭 인포패널 열렸을때, 인포패널 외 다른 포인트 클릭시, 인포패널 닫히게, 진화탭에서 사용했던 레이캐스트 필터(CloseOnPointer.cs) 재사용하여 구현

--- Assets/Scripts/Yun/EvolTap/CloseOnPointer.cs
+++ Assets/Scripts/Yun/EvolTap/CloseOnPointer.cs
@@ -10,35 +10,36 @@ using UnityEngine.EventSystems;
 /// 인포패널이 열려있을때만 이벤트 먹게끔 활성화 하고,
 /// 그리고 이벤트 먹으면 인포패널 닫히고, 해당패널도 닫히는 식으로
 /// -했다가 버튼도 안눌림. 버튼은 예외처리 할것
+/// ++장비탭에서도 쓸수 있게 변경
 /// </summary>
 public class CloseOnPointer : MonoBehaviour, IPointerDownHandler, ICanvasRaycastFilter
 {
-    [Header("진화 인포패널")]
-    [SerializeField] private GameObject evolInfoPanel;
+    [Header("인포패널(닫힐 대상)")]
+    [SerializeField] private GameObject infoPanel;
 
     [Header("닫기 입력 받을 이벤트패널")]
     [SerializeField] private GameObject closePointPanel;
 
-    [Header("클릭을 허용할 버튼-위치")]
-    [SerializeField] private RectTransform evolButtonRect;
+    [Header("클릭을 허용할 버튼/위치")]
+    [SerializeField] private RectTransform buttonRect;
 
     /// <summary>
     /// 버튼영역 안이면 false, 밖이면 true, false일때 버튼클릭가능
     /// </summary>
-    /// <param name="sp"></param>
+    /// <param name="screenPoint"></param>
     /// <param name="eventCamera"></param>
     /// <returns></returns>
     /// <exception cref="System.NotImplementedException"></exception>
     public bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
     {
-        bool isInsideButton = RectTransformUtility.RectangleContainsScreenPoint(evolButtonRect, screenPoint, eventCamera);
+        bool isInsideButton = RectTransformUtility.RectangleContainsScreenPoint(buttonRect, screenPoint, eventCamera);
         //버튼 안에서 레이캐스트 받지 않게
         return !isInsideButton;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        evolInfoPanel.SetActive(false);
+        infoPanel.SetActive(false);
         closePointPanel.SetActive(false);
     }
 }
