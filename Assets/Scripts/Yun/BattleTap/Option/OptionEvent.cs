a4753641e445d5aafcef29cc3c090e19560432ea
ghost-backseat-driver <ysl2323@naver.com>
2025-12-30 16:31:56 +0900

    comment : OptionEvent.cs 에 옵션패널 이벤트 구독을 사운드매니저, 전투씬에서 어떤식으로 할지 코멘트 추가

--- Assets/Scripts/Yun/BattleTap/Option/OptionEvent.cs
+++ Assets/Scripts/Yun/BattleTap/Option/OptionEvent.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 /// </summary>
 public static class OptionEvent
 {
-    //옵션 변경 이벤트 넣어주고
+    //옵션 변경 이벤트 넣어주고 -구독자들이 구독할거
     public static event Action<OptionToggleControl.ToggleOptionType, bool> OnOptionChanged;
 
     //옵션 토글 컨트롤로 넘겨줄거
@@ -23,3 +23,45 @@ public static class OptionEvent
         }
     }
 }
+
+/*
+ 외부에서 이벤트 구독할때 예시입니다!
+사운드 매니저에서 쓴다면,
+public class SoundManager : 모노비하이비어
+{
+    Private void OnEnabel() 활성화 때 구독
+    {
+        OptionEvent.OnOptionChanged += 옵션변경이벤트 함수 참조 -함수 자체를 참조하는거라 ()필요없슴다!
+    }
+
+    private void OnDisable() 비활성화 때 구독해제
+    {
+        OptionEvent.OnOptionChanged += 옵션변경이벤트 함수 참조 
+    }
+
+    //아마 사운드 매니저는 DDL 일거니까.. 활성화 비활성화 때 구독,구독해제 말고,
+    //그냥 시작시 start 쪽에서 구독만 해놓으면 될거 같슴다 위에는 전투씬에도 넣어야해서 예시로 썼어요
+    
+    //옵션 변경 이벤트 처리할 함수 하나
+    private void 옵션변경이벤트 함수(OptionToggleControl.ToggleOptionType type, bool value)
+    {
+        //사운드중 SFX라면,
+        if(type == OptionToggleControl.ToggleOptionType.SFX)
+        {
+            여기에 SFX 켜졌을때/안켜졌을때 조건분기
+            if(value == true)
+            {
+                //sfx켜는 로직
+            }
+            else
+            {
+                //sfx끄는 로직
+            }
+        }
+        //사운드중 BGM 이라면.. 위랑 같은식으로
+    }
+}
+대충 이런식으로? 
+
+ */
+
