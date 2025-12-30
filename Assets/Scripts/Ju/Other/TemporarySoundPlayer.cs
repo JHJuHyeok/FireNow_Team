7c3403336f5c63696c606ed447cbc6b24d15b464
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2025-12-30 13:45:51 +0900

    feat : 사운드 호출 시 생성될 오브젝트용 스크립트 작성

---
+++ Assets/Scripts/Ju/Other/TemporarySoundPlayer.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(AudioSource))]
+public class TemporarySoundPlayer : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public string ClipName { get { return audioSource.clip.name; } }
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayAudio(AudioMixerGroup audioMixer, float delay, bool isLoop)
+    {
+        audioSource.outputAudioMixerGroup = audioMixer;
+        audioSource.loop = isLoop;
+        audioSource.Play();
+
+        if(!isLoop)
+        {
+            //여기에 코루틴
+            CoDestroyWhenFinish(audioSource.clip.length);
+        }
+    }
+
+    public void InitSound(AudioClip clip)
+    {
+        audioSource.clip = clip;
+    }
+
+    private IEnumerator CoDestroyWhenFinish(float clipLength)
+    {
+        yield return new WaitForSeconds(clipLength);
+
+        Destroy(gameObject);
+    }
+}
